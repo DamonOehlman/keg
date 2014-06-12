2a6cd008463a5b6925d3cb0c3bbf717c8a8ab746
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 22:42:40 +1000

    Test GET request for item succeeds

--- test/deploy.js
+++ test/deploy.js
@@ -23,8 +23,21 @@ module.exports = function(registry, test) {
     request.put('/testpack@0.0.0').send({ name: 'Fred' }).expect(200, t.ifError);
   });
 
+  test('can get the created thing', function(t) {
+    t.plan(1);
+    request
+      .get('/testpack@0.0.0')
+      .expect('Content-Type', /json/)
+      .expect(200, { name: 'Fred' }, t.ifError);
+  });
+
   test('put an existing valid thing with a payload fails', function(t) {
     t.plan(1);
     request.put('/testpack@0.0.0').send({ name: 'Bob' }).expect(412, t.ifError);
+  });
+
+  test('put a new valid thing succeeds', function(t) {
+    t.plan(1);
+    request.put('/testpack@0.0.1').send({ name: 'Frederick' }).expect(200, t.ifError);
   });
 };
