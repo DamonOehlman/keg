8d843c3c038d40d1211d1ffcd528e86760d80a45
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 22:42:55 +1000

    Implemented reading from db

---
+++ methods/get.js
@@ -0,0 +1,23 @@
+var abort = require('../lib/abort');
+var debug = require('debug')('keg:get');
+var p = require('padded-semver');
+
+module.exports = function(registry, opts) {
+  var db = registry.db;
+
+  return function(req, res, package) {
+    var key = package.name + ':' + p.pad(package.version);
+
+    db.get(key, { valueEncoding: 'utf-8' }, function(err, value) {
+      // if it already exists, then complain
+      if (err) {
+        return abort(res, err.notFound ? 'notFound' : '', err);
+      }
+
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.end(value);
+    });
+  };
+};
--- methods/index.js
+++ methods/index.js
@@ -1,7 +1,7 @@
 module.exports = function(registry, opts) {
   var handlers = {};
 
-  ['put'].forEach(function(method) {
+  ['get', 'put'].forEach(function(method) {
     handlers[method] = require('./' + method + '.js')(registry, opts);
   });
 
