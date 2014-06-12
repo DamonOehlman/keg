8bc8e75427fa337c7d492965ddc7e3571139263d
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 21:01:34 +1000

    Attach method handlers correctly

--- methods/index.js
+++ methods/index.js
@@ -2,7 +2,7 @@ module.exports = function(registry, opts) {
   var handlers = {};
 
   ['put'].forEach(function(method) {
-    exports[method] = require('./' + method + '.js')(registry, opts);
+    handlers[method] = require('./' + method + '.js')(registry, opts);
   });
 
   return handlers;
