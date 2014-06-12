d03721834116a73a767a6cca33d7190d3debbc7e
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 21:01:13 +1000

    Use concat-stream for request body handling

--- methods/put.js
+++ methods/put.js
@@ -1,10 +1,16 @@
+var abort = require('../lib/abort');
 var debug = require('debug')('keg:put');
+var concat = require('concat-stream');
 
 module.exports = function(registry, opts) {
   var db = registry.db;
 
-  return function(req, res) {
-    req.on('end', function() {
-    });
+  return function(req, res, data) {
+    debug('attempting to handle request: ' + req.url);
+    req.pipe(concat(function(data) {
+      if (Array.isArray(data) && data.length === 0) {
+        return abort(res, 'emptyPayload');
+      }
+    }));
   };
 };
