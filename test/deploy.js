e4f97b9268758fe4a803c02b4bb4fc66958d5bf1
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 22:29:26 +1000

    Test immutability

--- test/deploy.js
+++ test/deploy.js
@@ -22,4 +22,9 @@ module.exports = function(registry, test) {
     t.plan(1);
     request.put('/testpack@0.0.0').send({ name: 'Fred' }).expect(200, t.ifError);
   });
+
+  test('put an existing valid thing with a payload fails', function(t) {
+    t.plan(1);
+    request.put('/testpack@0.0.0').send({ name: 'Bob' }).expect(412, t.ifError);
+  });
 };
