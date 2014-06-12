3f1ff52da2629870e50bbf2b8022949b4907081a
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 21:02:04 +1000

    Tweak deploy test structure and add empty payload test

--- test/deploy.js
+++ test/deploy.js
@@ -1,17 +1,24 @@
 var request = require('supertest');
 
 module.exports = function(registry, test) {
+  var request = require('supertest')(registry.url);
+
   test('attempt to put unversioned thing fails', function(t) {
     t.plan(1);
-    request(registry.url)
-      .put('/testpack')
+    request.put('/testpack')
       .expect(500, t.pass.bind(t, 'done'));
   });
 
   test('attempt to put a thing with an invalid version string', function(t) {
     t.plan(1);
-    request(registry.url)
-      .put('/testpack@adsf')
+    request.put('/testpack@adsf')
+      .expect(500, t.pass.bind(t, 'done'));
+  });
+
+  test('attempt to put a valid thing with no data fails', function(t) {
+    t.plan(1);
+    request
+      .put('/testpack@0.0.0')
       .expect(500, t.pass.bind(t, 'done'));
   });
 };
