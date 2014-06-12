017b57635bae63e8f0223f3a72aca4ca885c061d
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-12 21:54:52 +1000

    Test for ok deploy

--- test/deploy.js
+++ test/deploy.js
@@ -5,20 +5,21 @@ module.exports = function(registry, test) {
 
   test('attempt to put unversioned thing fails', function(t) {
     t.plan(1);
-    request.put('/testpack')
-      .expect(500, t.pass.bind(t, 'done'));
+    request.put('/testpack').expect(500, t.ifError);
   });
 
   test('attempt to put a thing with an invalid version string', function(t) {
     t.plan(1);
-    request.put('/testpack@adsf')
-      .expect(500, t.pass.bind(t, 'done'));
+    request.put('/testpack@adsf').expect(500, t.ifError);
   });
 
   test('attempt to put a valid thing with no data fails', function(t) {
     t.plan(1);
-    request
-      .put('/testpack@0.0.0')
-      .expect(500, t.pass.bind(t, 'done'));
+    request.put('/testpack@0.0.0').expect(500, t.ifError);
+  });
+
+  test('put a valid thing with a payload succeeds', function(t) {
+    t.plan(1);
+    request.put('/testpack@0.0.0').send({ name: 'Fred' }).expect(200, t.ifError);
   });
 };
