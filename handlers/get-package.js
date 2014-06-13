d6d23cbb80efb8a119da2fbe6be98a2c3179a568
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-14 00:07:50 +1000

    Updated get package handler to get the latest version of a package

--- handlers/get-package.js
+++ handlers/get-package.js
@@ -5,9 +5,41 @@ var svkey = require('semver-key');
 module.exports = function(registry, opts) {
   var db = registry.db;
 
-  return function(req, res, package) {
-    var key = package.name + '!' + svkey(package.version);
+  function getLatestItem(req, res, package) {
+    var reader = db.createReadStream({
+      start: package.name,
+      limit: 1
+    });
+    var writtenHead = false;
 
+    function writeHead(data) {
+      var parts = data.key.split('!');
+
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        'x-keg-version': svkey.unpack(parts[1])
+      });
+    }
+
+    reader
+      .on('data', function(data) {
+        writtenHead = writtenHead || writeHead(data);
+        res.write(data.value);
+      })
+      .on('end', function() {
+        res.end();
+      });
+  }
+
+  return function(req, res, package) {
+    var key;
+
+    // if we have no package version, get the latest item
+    if (! package.version) {
+      return getLatestItem(req, res, package);
+    }
+
+    key = package.name + '!' + svkey(package.version);
     db.get(key, { valueEncoding: 'utf-8' }, function(err, value) {
       // if it already exists, then complain
       if (err) {
