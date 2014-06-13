838df6b6c06f37a8c30c43a5b48f903e84b00589
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-14 00:07:16 +1000

    Additional deployment and read tests

--- test/deploy.js
+++ test/deploy.js
@@ -15,29 +15,72 @@ module.exports = function(registry, test) {
 
   test('attempt to put a valid thing with no data fails', function(t) {
     t.plan(1);
-    request.put('/testpack/0.0.0').expect(500, t.ifError);
+    request.put('/testpack/1.0.0').expect(500, t.ifError);
   });
 
   test('put a valid thing with a payload succeeds', function(t) {
     t.plan(1);
-    request.put('/testpack/0.0.0').send({ name: 'Fred' }).expect(200, t.ifError);
+    request.put('/testpack/1.0.0').send({ name: 'Fred' }).expect(200, t.ifError);
   });
 
   test('can get the created thing', function(t) {
     t.plan(1);
     request
-      .get('/testpack/0.0.0')
+      .get('/testpack/1.0.0')
       .expect('Content-Type', /json/)
       .expect(200, { name: 'Fred' }, t.ifError);
   });
 
   test('put an existing valid thing with a payload fails', function(t) {
     t.plan(1);
-    request.put('/testpack/0.0.0').send({ name: 'Bob' }).expect(412, t.ifError);
+    request.put('/testpack/1.0.0').send({ name: 'Bob' }).expect(412, t.ifError);
   });
 
   test('put a new valid thing succeeds', function(t) {
     t.plan(1);
-    request.put('/testpack/0.0.1').send({ name: 'Frederick' }).expect(200, t.ifError);
+    request.put('/testpack/8.0.0').send({ name: 'Frederick' }).expect(200, t.ifError);
+  });
+
+  test('can get the new thing', function(t) {
+    t.plan(1);
+    request
+      .get('/testpack/8.0.0')
+      .expect('Content-Type', /json/)
+      .expect(200, { name: 'Frederick' }, t.ifError);
+  });
+
+  test('can deploy a thing which is a patch of an earlier version', function(t) {
+    t.plan(1);
+    request.put('/testpack/1.0.5').send({ name: 'Fred', age: 50 }).expect(200, t.ifError);
+  });
+
+  test('can get the patched thing', function(t) {
+    t.plan(1);
+    request
+      .get('/testpack/1.0.5')
+      .expect('Content-Type', /json/)
+      .expect(200, { name: 'Fred', age: 50 }, t.ifError);
+  });
+
+  test('can deploy a thing that might create lexicographic chaos', function(t) {
+    t.plan(1);
+    request.put('/testpack/10.3.5').send({ name: 'F', age: 51 }).expect(200, t.ifError);
+  });
+
+  test('can get that new troublesome thing', function(t) {
+    t.plan(1);
+    request
+      .get('/testpack/10.3.5')
+      .expect('Content-Type', /json/)
+      .expect(200, { name: 'F', age: 51 }, t.ifError);
+  });
+
+  test('can get the latest thing', function(t) {
+    t.plan(1);
+    request
+      .get('/testpack')
+      .expect('Content-Type', /json/)
+      .expect('x-keg-version', '10.3.5')
+      .expect(200, { name: 'F', age: 51 }, t.ifError);
   });
 };
