81c9bea5868ac92ce272218a4ffd292256e74e4b
Damon Oehlman <damon.oehlman@gmail.com>
2015-09-21 19:08:32 +1000

    fixed range tests

--- test/range.js
+++ test/range.js
@@ -10,28 +10,28 @@ module.exports = function(registry, test) {
       .expect(200, { name: 'Fred', age: 50 }, t.ifError);
   });
 
-  test('request for testpack ^0.2.0 returns version 0.2.3', function(t) {
+  test('request for testpack 0.2.x returns version 0.2.3', function(t) {
     t.plan(1);
     request
-      .get('/main/testpack/^0.2.0')
+      .get('/main/testpack/0.2.x')
       .expect('Content-Type', /json/)
       .expect('x-keg-version', '0.2.3')
       .expect(200, { age: 2 }, t.ifError);
   });
 
-  test('request for testpack ^0.3.0 returns version 0.3.6', function(t) {
+  test('request for testpack 0.3.x returns version 0.3.6', function(t) {
     t.plan(1);
     request
-      .get('/main/testpack/^0.3.0')
+      .get('/main/testpack/0.3.x')
       .expect('Content-Type', /json/)
       .expect('x-keg-version', '0.3.6')
       .expect(200, { age: 3 }, t.ifError);
   });
 
-  test('request for testpack ^0.4.0 returns 404', function(t) {
+  test('request for testpack 0.4.x returns 404', function(t) {
     t.plan(1);
     request
-      .get('/main/testpack/^0.4.0')
+      .get('/main/testpack/0.4.x')
       .expect(404, t.ifError);
   });
 };
